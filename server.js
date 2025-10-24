fd5498643c1e27821ff1e8e561c4ceb4a33b318b
dailyoddds100-prog <dailyoddds100@gmail.com>
2025-10-24 04:32:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use(express.urlencoded({ extended: true })); // handles form data
 app.use(express.static('public')); // serves index.html from /public
 
